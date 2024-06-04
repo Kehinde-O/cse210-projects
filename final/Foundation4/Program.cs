5f38c83662b631a4206e36254da7a7b9b238ff78
kehinde-omotoso-78financials <110571589+kehinde-omotoso-78financials@users.noreply.github.com>
2024-06-04 23:49:30 +0100

    Completed Youtube Video Project

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,40 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Video> videos = new List<Video>();
+        List<Video> _videos = new List<Video>();
 
-        Video video1 = new Video("Python OPTIMIZATION Trick!!", "Frank Kane", 340);
-        video1.AddComment(new Comment("This is a great video!","John Doe"));
-        video1.AddComment(new Comment("I love this!","Jane Doe"));
-        video1.AddComment(new Comment("I have to say, this is the best video I have ever seen!","Sam Smith"));
-        video1.AddComment(new Comment("Another alternative is to create a default parameter for the function.","Anna Smith"));
+        Video _video1 = new Video("Python OPTIMIZATION Trick!!", "Frank Kane", 340);
+        _video1.AddComment(new Comment("This is a great video!","John Doe"));
+        _video1.AddComment(new Comment("I love this!","Jane Doe"));
+        _video1.AddComment(new Comment("I have to say, this is the best video I have ever seen!","Sam Smith"));
+        _video1.AddComment(new Comment("Another alternative is to create a default parameter for the function.","Anna Smith"));
 
-        videos.Add(video1);
+        _videos.Add(_video1);
 
-        Video video2 = new Video("C# OPTIMIZATION Trick!!", "Tolulope Atomori", 120);
-        video2.AddComment(new Comment("Definitely use this trick to run out of variable names","@tahaahmedmallick2008"));
-        video2.AddComment(new Comment("there's the `global` keyword, or `nonlocal` for nested functions","@sami9323"));
-        video2.AddComment(new Comment("If you need enough performance that something like this becomes worth it, you now are in need of a higher performance language.","@pixelprizm"));
-        video2.AddComment(new Comment("Never mention 'performance' and 'Python' in one item!","@hamkaasburger6142"));
+        Video _video2 = new Video("C# OPTIMIZATION Trick!!", "Tolulope Atomori", 120);
+        _video2.AddComment(new Comment("Definitely use this trick to run out of variable names","@tahaahmedmallick2008"));
+        _video2.AddComment(new Comment("there's the `global` keyword, or `nonlocal` for nested functions","@sami9323"));
+        _video2.AddComment(new Comment("If you need enough performance that something like this becomes worth it, you now are in need of a higher performance language.","@pixelprizm"));
+        _video2.AddComment(new Comment("Never mention 'performance' and 'Python' in one item!","@hamkaasburger6142"));
 
-        videos.Add(video2);
+        _videos.Add(_video2);
 
-        Video video3 = new Video("Every Kind of Bridge Explained in 15 Minutes", "Practical Engineering", 3000);
-        video3.AddComment(new Comment("Oh, you like bridges? Name every bridge.","@muenstercheese"));
-        video3.AddComment(new Comment("Y'know I'm something of a bridge engineer myself","@SoulDuckling126"));
-        video3.AddComment(new Comment("Here's a drinking game for you: take a shot every time a bridge from the Pittsburgh area is displayed in this video","@FltCaptAlan"));
+        Video _video3 = new Video("Every Kind of Bridge Explained in 15 Minutes", "Practical Engineering", 3000);
+        _video3.AddComment(new Comment("Oh, you like bridges? Name every bridge.","@muenstercheese"));
+        _video3.AddComment(new Comment("Y'know I'm something of a bridge engineer myself","@SoulDuckling126"));
+        _video3.AddComment(new Comment("Here's a drinking game for you: take a shot every time a bridge from the Pittsburgh area is displayed in this video","@FltCaptAlan"));
 
-        videos.Add(video3);
+        _videos.Add(_video3);
 
         Console.Clear();
-        foreach(Video video in videos){
-            Console.WriteLine($"Video Title: {video.GetTitle()} by {video.GetAuthor()}. video is {video.GetLength()} seconds long.");
-            Console.WriteLine($"Uploaded by: {video.GetAuthor()}");
-            Console.WriteLine($"Length: {video.GetLength()} seconds long.");
-            Console.WriteLine($"Comments({video.GetNumberOfComments()}):");
+        foreach(Video _video in _videos){
+            Console.WriteLine($"Video Title: {_video.GetTitle()} by {_video.GetAuthor()}. video is {_video.GetLength()} seconds long.");
+            Console.WriteLine($"Uploaded by: {_video.GetAuthor()}");
+            Console.WriteLine($"Length: {_video.GetLength()} seconds long.");
+            Console.WriteLine();
+            Console.WriteLine($"Comments({_video.GetNumberOfComments()}):");
             int commentIndex = 1;
-            foreach(Comment comment in video.GetComments()){
-                Console.WriteLine($" ({commentIndex}). {comment.GetName()}: {comment.GetText()}");
+            foreach(Comment _comment in _video.GetComments()){
+                Console.WriteLine($" ({commentIndex}). {_comment.GetName()}: {_comment.GetText()}");
                 commentIndex++;
             }
             Console.WriteLine();
